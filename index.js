4397cc9d8c1fd6b84e515246511ad0867a7a67cc
asafuli <asafeli@gmail.com>
2018-09-17 11:50:25 +0300

    Initial commit

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ require("./startup/config")();
 require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
-const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
-);
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
--- routes/movies.js
+++ routes/movies.js
@@ -17,6 +17,7 @@ router.get("/", async (req, res) => {
 
 router.post("/", [auth], async (req, res) => {
   const { error } = validate(req.body);
+  console.log(22);
   if (error) return res.status(400).send(error.details[0].message);
 
   const genre = await Genre.findById(req.body.genreId);
